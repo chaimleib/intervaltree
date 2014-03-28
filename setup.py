d7b6ba85b73a21cc9a384484ab14b9c44e1adcb9
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-03-27 23:29:15 -0700

    forgot to add setup.*

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = 'intervaltree',
+    packages = ['intervaltree'],
+    version = '0.1',
+    description = 'A mutable, self-balancing interval tree.\n' \
+        'Queries may be by point, by range overlap, or by range envelopment.',
+    author = 'Chaim-Leib Halbert',
+    author_email = 'chaim.leib.halbert@gmail.com',
+    url = 'https://github.com/MusashiAharon/PyIntervaTree',
+    download_url = 'https://github.com/Musashiaharon/PyIntervalTree/tarball/0.1',
+    keywords = ['interval', 'tree', 'data', 'structure'],
+    classifiers = [],
+    )
+
