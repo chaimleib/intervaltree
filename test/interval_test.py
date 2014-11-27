5fc624b4f3f1b9734e1634535efa4b5d380a4d9d
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-11-26 17:26:35 -0800

    add: test Interval.copy() and .__reduce__()

--- test/interval_test.py
+++ test/interval_test.py
@@ -29,3 +29,16 @@ def test_isnull():
     iv = Interval(1,0)
     assert iv.is_null()
 
+def test_copy():
+    iv0 = Interval(1, 2, 3)
+    iv1 = iv0.copy()
+    assert iv1.begin == iv0.begin
+    assert iv1.end == iv0.end
+    assert iv1.data == iv0.data
+    assert iv1 == iv0
+
+    iv2 = pickle.loads(pickle.dumps(iv0))
+    assert iv2.begin == iv0.begin
+    assert iv2.end == iv0.end
+    assert iv2.data == iv0.data
+    assert iv2 == iv0
