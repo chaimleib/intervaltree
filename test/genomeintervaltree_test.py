a4b8ef29863932b9a052a073604dab076e5061ec
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-11-30 21:27:25 -0800

    removed other broken urls from genome test

--- test/genomeintervaltree_test.py
+++ test/genomeintervaltree_test.py
@@ -39,14 +39,12 @@ def test_knownGene():
 def test_ensGene():
     # Smoke-test we can at least read ensGene.
     ensGene_url = 'http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/ensGene.txt.gz'
-    ensGene_url = 'http://kt.era.ee/distribute/pyintervaltree/ensGene.txt.gz'
     ensGene = GenomeIntervalTree.from_table(url=ensGene_url, mode='cds', parser=UCSCTable.ENS_GENE)
     assert len(ensGene) == 204940
 
 def test_refGene():
     # Smoke-test for refGene
     refGene_url = 'http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/refGene.txt.gz'
-    refGene_url = 'http://kt.era.ee/distribute/pyintervaltree/refGene.txt.gz'
     refGene = GenomeIntervalTree.from_table(url=refGene_url, mode='tx', parser=UCSCTable.REF_GENE)
     assert len(refGene) == 52289  # NB: Some time ago it was 50919, hence it seems the table data changes on UCSC and eventually the mirror and UCSC won't be the same.
 
