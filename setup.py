c2ff2358228af79487818f03da04116219743995
Konstantin Tretyakov <kt@ut.ee>
2014-12-02 03:11:46 +0200

    Typo

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-)'''
+'''
 PyIntervalTree: A mutable, self-balancing interval tree. Queries may be by point, by range overlap, or by range envelopment.
 
 Note that "python setup.py test" invokes pytest on the package. With appropriately
