07eaaf3754dcd0da863679ce89169bd69979bc11
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-12-17 23:54:27 -0800

    merged intervaltree/ from hotfix

--- intervaltree/interval.py
+++ intervaltree/interval.py
@@ -23,12 +23,20 @@ from numbers import Number
 from collections import namedtuple
 
 
+# noinspection PyBroadException
 class Interval(namedtuple('IntervalBase', ['begin', 'end', 'data'])):
     __slots__ = ()  # Saves memory, avoiding the need to create __dict__ for each interval
     def __new__(cls, begin, end, data=None):
         return super(Interval, cls).__new__(cls, begin, end, data)
     
     def overlaps(self, begin, end=None):
+        """
+        Whether the interval overlaps the given point, range or Interval.
+        :param begin: beginning point of the range, or the point, or an Interval
+        :param end: end point of the range. Optional if not testing ranges.
+        :return: True or False
+        :rtype: bool
+        """
         if end is not None:
             return (
                 (begin <= self.begin < end) or
@@ -36,21 +44,39 @@ class Interval(namedtuple('IntervalBase', ['begin', 'end', 'data'])):
                 (self.begin <= begin < self.end) or
                 (self.begin < end <= self.end)
             )
-        elif isinstance(begin, Number):
-            return self.contains_point(begin)
-        else:   # duck-typed interval
+        try:
             return self.overlaps(begin.begin, begin.end)
-    
+        except:
+            return self.contains_point(begin)
+
     def contains_point(self, p):
+        """
+        Whether the Interval contains p.
+        :param p: a point
+        :return: True or False
+        :rtype: bool
+        """
         return self.begin <= p < self.end
     
     def range_matches(self, other):
+        """
+        Whether the begins equal and the ends equal. Compare __eq__().
+        :param other: Interval
+        :return: True or False
+        :rtype: bool
+        """
         return (
             self.begin == other.begin and 
             self.end == other.end
         )
     
     def contains_interval(self, other):
+        """
+        Whether other is contained in this Interval.
+        :param other: Interval
+        :return: True or False
+        :rtype: bool
+        """
         return (
             self.begin <= other.begin and
             self.end >= other.end
@@ -60,29 +86,57 @@ class Interval(namedtuple('IntervalBase', ['begin', 'end', 'data'])):
         """
         Returns the size of the gap between intervals, or 0 
         if they touch or overlap.
+        :param other: Interval or point
+        :return: distance
+        :rtype: Number
         """
         if self.overlaps(other):
             return 0
-        elif self.begin < other.begin:
-            return other.begin - self.end
-        else:
-            return self.begin - other.end
+        try:
+            if self.begin < other.begin:
+                return other.begin - self.end
+            else:
+                return self.begin - other.end
+        except:
+            if self.end < other:
+                return other - self.end
+            else:
+                return self.begin - other
 
     def is_null(self):
         """
-        Returns True if nothing is contained in this interval, i.e. end <= begin.
+        Whether this equals the null interval.
+        :return: True if end <= begin else False
+        :rtype: bool
         """
         return self.begin >= self.end
 
     def length(self):
+        """
+        The distance covered by this Interval.
+        :return: length
+        :type: Number
+        """
         if self.is_null():
             return 0
         return self.end - self.begin
 
     def __hash__(self):
+        """
+        Depends on begin and end only.
+        :return: hash
+        :rtype: Number
+        """
         return hash((self.begin, self.end))
 
     def __eq__(self, other):
+        """
+        Whether the begins equal, the ends equal, and the data fields
+        equal. Compare range_matches().
+        :param other: Interval
+        :return: True or False
+        :rtype: bool
+        """
         return (
             self.begin == other.begin and
             self.end == other.end and
@@ -90,8 +144,23 @@ class Interval(namedtuple('IntervalBase', ['begin', 'end', 'data'])):
         )
 
     def __cmp__(self, other):
+        """
+        Tells whether other sorts before, after or equal to this
+        Interval.
+
+        Sorting is by begins, then by ends, then by data fields.
+
+        If data fields are not both sortable types, data fields are
+        compared alphabetically by type name.
+        :param other: Interval
+        :return: -1, 0, 1
+        :rtype: int
+        """
         s = self[0:2]
-        o = other[0:2]
+        try:
+            o = other[0:2]
+        except:
+            o = other
         if s != o:
             return -1 if s < o else 1
         try:
@@ -106,15 +175,30 @@ class Interval(namedtuple('IntervalBase', ['begin', 'end', 'data'])):
             return -1 if s < o else 1
         
     def __lt__(self, other):
+        """
+        Less than operator. Parrots __cmp__()
+        :param other: Interval or point
+        :return: True or False
+        :rtype: bool
+        """
         return self.__cmp__(other) < 0
 
     def __gt__(self, other):
+        """
+        Greater than operator. Parrots __cmp__()
+        :param other: Interval or point
+        :return: True or False
+        :rtype: bool
+        """
         return self.__cmp__(other) > 0
 
     def _get_fields(self):
-        """Used by str, unicode, repr and __reduce__.
+        """
+        Used by str, unicode, repr and __reduce__.
 
         Returns only the fields necessary to reconstruct the Interval.
+        :return: reconstruction info
+        :rtype: tuple
         """
         if self.data is not None:
             return self.begin, self.end, self.data
@@ -122,6 +206,11 @@ class Interval(namedtuple('IntervalBase', ['begin', 'end', 'data'])):
             return self.begin, self.end
     
     def __repr__(self):
+        """
+        Executable string representation of this Interval.
+        :return: string representation
+        :rtype: str
+        """
         if isinstance(self.begin, Number):
             s_begin = str(self.begin)
             s_end = str(self.end)
@@ -136,10 +225,17 @@ class Interval(namedtuple('IntervalBase', ['begin', 'end', 'data'])):
     __str__ = __repr__
 
     def copy(self):
+        """
+        Shallow copy.
+        :return: copy of self
+        :rtype: Interval
+        """
         return Interval(self.begin, self.end, self.data)
     
     def __reduce__(self):
         """
         For pickle-ing.
+        :return: pickle data
+        :rtype: tuple
         """
         return Interval, self._get_fields()
