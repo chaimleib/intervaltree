4d234b8394874cec4afb1489539b6cdb6a4d9bc3
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-05-07 02:26:19 -0700

    add: ability to use a reducer function for the data field in merge_overlaps()

--- intervaltree/intervaltree.py
+++ intervaltree/intervaltree.py
@@ -24,6 +24,7 @@ from .node import Node
 from numbers import Number
 import collections
 from sortedcontainers import SortedDict
+from copy import copy
 from warnings import warn
 
 try:
@@ -633,28 +634,63 @@ class IntervalTree(collections.MutableSet):
 
         self.__init__(new_ivs)
 
-    def merge_overlaps(self):
+    def merge_overlaps(self, data_reducer=None, data_initializer=None):
         """
-        Finds all intervals with overlapping ranges and merges them into a single interval
+        Finds all intervals with overlapping ranges and merges them
+        into a single interval. If provided, uses data_reducer and
+        data_initializer with similar semantics to Python's built-in
+        reduce(reducer_func[, initializer]), as follows:
+
+        If data_reducer is set to a function, combines the data
+        fields of the Intervals with
+            current_reduced_data = data_reducer(current_reduced_data, new_data)
+        If data_reducer is None, the merged Interval's data
+        field will be set to None, ignoring all the data fields
+        of the merged Intervals.
+
+        On encountering the first Interval to merge, if
+        data_initializer is None (default), uses the first
+        Interval's data field as the first value for
+        current_reduced_data. If data_initializer is not None,
+        current_reduced_data is set to a shallow copy of
+        data_initiazer created with
+            copy.copy(data_initializer).
+
         Completes in O(n*logn).
         """
-
         if not self:
             return
 
         sorted_intervals = sorted(self.all_intervals)  # get sorted intervals
-        merged = list()
+        merged = []
+        # use mutable object to allow new_series() to modify it
+        current_reduced = [None]
+        higher = None  # iterating variable, which new_series() needs access to
+
+        def new_series():
+            if data_initializer is None:
+                current_reduced[0] = higher.data
+                merged.append(higher)
+                return
+            else:  # data_initializer is not None
+                current_reduced[0] = copy(data_initializer)
+                current_reduced[0] = data_reducer(current_reduced[0], higher.data)
+                merged.append(Interval(higher.begin, higher.end, current_reduced[0]))
 
         for higher in sorted_intervals:
-            if not merged:
-                merged.append(higher)
-            else:
+            if merged:  # series already begun
                 lower = merged[-1]
-                if higher.begin <= lower.end:
+                if higher.begin <= lower.end:  # should merge
                     upper_bound = max(lower.end, higher.end)
-                    merged[-1] = Interval(lower.begin, upper_bound)
+                    if data_reducer is not None:
+                        current_reduced[0] = data_reducer(current_reduced[0], higher.data)
+                    else:  # annihilate the data, since we don't know how to merge it
+                        current_reduced[0] = None
+                    merged[-1] = Interval(lower.begin, upper_bound, current_reduced[0])
                 else:
-                    merged.append(higher)
+                    new_series()
+            else:  # not merged; is first of Intervals to merge
+                new_series()
 
         self.__init__(merged)
 
